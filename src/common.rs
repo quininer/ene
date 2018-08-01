d349d8e9e5ae8f47d5416f17200b197851cf2559
quininer <quininer@live.com>
2018-08-01 08:28:46 +0800

    revert seckey 0.10

--- src/common.rs
+++ src/common.rs
@@ -91,13 +91,11 @@ impl Serde for Cbor {
     type Error = CborError;
 
     fn to_vec<T: Serialize>(value: &T) -> Result<Vec<u8>, error::Error<Self::Error>> {
-        cbor::to_vec(value)
-            .map_err(|err| error::Error::Format(err.into()))
+        cbor::to_vec(value).map_err(error::Error::Format)
     }
 
     fn from_slice<'a, T: Deserialize<'a>>(slice: &'a [u8]) -> Result<T, error::Error<Self::Error>> {
-        cbor::from_slice(slice)
-            .map_err(|err| error::Error::Format(err.into()))
+        cbor::from_slice(slice).map_err(error::Error::Format)
     }
 }
 
